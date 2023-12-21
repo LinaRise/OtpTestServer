e78131996817669d3fefd56ca5debfa65e5da997
Alina <galeeva@deck.lc>
2023-12-21 14:03:50 +0300

    server side

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,13 @@
 import entity.Otp
-import io.ktor.client.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
-import java.awt.SystemColor.text
 
 const val API_PATH = "v1"
 fun main() {
